27c54d48c17df993deebeaf5025bc5e431b573cf
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2020-09-10 11:52:55 +0200

    Toolbar Aktionen hinzugefügt
    Es wurden Aktionen implementiert, die ausgeführt werden, wenn ein Item aus der Toolbar ausgewählt wird.
    Auch dieses sind nur Platzhalter und müssen später genauer auf die App angepasst werden.

--- app/src/main/java/com/example/hwr_huschka/MainActivity.java
+++ app/src/main/java/com/example/hwr_huschka/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.hwr_huschka;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.toolbar_dropdown_menu, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.Settings:
+                Toast.makeText(this, "Settings pushed", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.impressum:
+                Toast.makeText(this, "Impressum pushed", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
