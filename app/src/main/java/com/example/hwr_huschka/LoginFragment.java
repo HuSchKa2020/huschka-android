e00a058e7bc7991e963cc10a8b2330df9491664b
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2020-09-21 15:37:52 +0200

    Login als Start Activity
    Logik zum wechseln zwischen registrieren und einloggen. Derzeit über Button Login ohne User prüfung zur Main Activity.

--- app/src/main/java/com/example/hwr_huschka/LoginFragment.java
+++ app/src/main/java/com/example/hwr_huschka/LoginFragment.java
@@ -1,6 +1,55 @@
 package com.example.hwr_huschka;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class LoginFragment extends Fragment {
+
+    TextView goToRegistration;
+    Button btnLogin;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login_login, container, false);
+
+        goToRegistration = v.findViewById(R.id.TVtoRegistration);
+        goToRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragmentLoginContainer, new RegistrationFragment(), "register");
+                ft.commit();
+
+                //Toast.makeText(getContext(), "You Clicke here", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnLogin = v.findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+
+        return v;
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this.getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/hwr_huschka/RegistrationFragment.java
+++ app/src/main/java/com/example/hwr_huschka/RegistrationFragment.java
@@ -1,4 +1,36 @@
 package com.example.hwr_huschka;
 
-public class RegistrationFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class RegistrationFragment extends Fragment {
+
+    TextView goToLogin;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login_registration, container, false);
+
+        goToLogin = v.findViewById(R.id.TVtoLogin);
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragmentLoginContainer, new LoginFragment(), "login");
+                ft.commit();
+            }
+        });
+
+        return v;
+    }
+
 }
