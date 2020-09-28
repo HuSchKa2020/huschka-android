04544d43a04a808c1552e56f5ad1d5b8b62843a9
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2020-09-28 19:57:31 +0200

    registrierung funktioniert lokal
    für online muss nur die IP in der Klasse Constants angepasst werden.

--- app/src/main/java/com/example/hwr_huschka/Constants.java
+++ app/src/main/java/com/example/hwr_huschka/Constants.java
@@ -2,7 +2,7 @@ package com.example.hwr_huschka;
 
 public class Constants {
 
-    public static final String ROOT_URL = "http://192.168.178.33/Android/v1/"; // muss noch angepasst werden
+    public static final String ROOT_URL = "http://192.168.178.38/v1/"; // muss noch angepasst werden
 
     public static final String URL_LOGIN_USER = ROOT_URL + "loginUser.php";
     public static final String URL_REGISTER_USER = ROOT_URL + "registerUser.php";
