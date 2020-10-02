ffea7a45282e28598fe695059c40530d0a6c8893
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2020-10-02 18:55:40 +0200

    ausloggen implementiert

--- app/src/main/java/com/example/hwr_huschka/Activities/MainActivity.java
+++ app/src/main/java/com/example/hwr_huschka/Activities/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -79,9 +81,25 @@ public class MainActivity extends AppCompatActivity {
             case R.id.impressum:
                 Toast.makeText(this, "Impressum pushed", Toast.LENGTH_LONG).show();
                 return true;
+            case R.id.logout:
+                clearSharedpreferences();
+                goToLogin();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void clearSharedpreferences(){
+        SharedPreferences sharedPreferences = getSharedPreferences("userdata", MODE_PRIVATE);
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        edit.clear();
+        edit.commit();
+    }
+
+    private void goToLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 
