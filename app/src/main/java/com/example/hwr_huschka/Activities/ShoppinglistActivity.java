d02cf703bed82084028a5c540e9b64c79ef335a9
derSchumann <der.schumann194@gmx.de>
2020-10-06 22:23:57 +0200

    Design ShoppingList & Update Registierung
    erster Versuch der ShoppingList - ListView muss noch überarbeitet werden
    
    Registrieung is hoffentlich endlich richtig!!

--- app/src/main/java/com/example/hwr_huschka/Activities/ShoppinglistActivity.java
+++ app/src/main/java/com/example/hwr_huschka/Activities/ShoppinglistActivity.java
@@ -3,14 +3,44 @@ package com.example.hwr_huschka.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.hwr_huschka.R;
 
+import java.util.ArrayList;
+
 public class ShoppinglistActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoppinglist);
+
+
+        listView= (ListView) findViewById(R.id.LV_shoppinglist_ProduktListe);
+
+        ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("Heinz Ketchup");
+        arrayList.add("Kekse");
+        arrayList.add("Schweinefilet");
+        arrayList.add("Chips");
+        arrayList.add("Goldkrone");
+        arrayList.add("Klopapier");
+        arrayList.add("Brötchen");
+        arrayList.add("Kartoffeln");
+
+        ArrayAdapter arrayAdapter= new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+
+
+
+
     }
+
+
 }
