6d7563626ff2dbac430cdca2f4757ca4db631f6f
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2020-10-12 10:42:13 +0200

    zwei ListViews: Eine zur Suche von Produkten, andere mit Produkten der Liste

--- app/src/main/java/com/example/hwr_huschka/Activities/AddProductsToListActivity.java
+++ app/src/main/java/com/example/hwr_huschka/Activities/AddProductsToListActivity.java
@@ -1,10 +1,14 @@
 package com.example.hwr_huschka.Activities;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.hwr_huschka.R;
+import com.example.hwr_huschka.klassen.Product;
 import com.example.hwr_huschka.klassen.ShoppingList;
+
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,20 @@ public class AddProductsToListActivity extends AppCompatActivity {
 
     ShoppingList shoppingList;
 
+    List<Product> checkedItems;
+
+    ListView listViewProdcts;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_produkte);
+        setContentView(R.layout.activity_addproducttoshoppinglist);
+
         shoppingList = (ShoppingList) this.getIntent().getSerializableExtra("shoppinglist");
+        checkedItems = shoppingList.getInhalt();
+
+        listViewProdcts = findViewById(R.id.produktListView);
+
 
     }
 }
