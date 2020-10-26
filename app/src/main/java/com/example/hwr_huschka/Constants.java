529d824327c8094bf15d72b6cf194b1c38a9bfed
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2020-10-26 10:46:06 +0100

    fix registerBug

--- app/src/main/java/com/example/hwr_huschka/Constants.java
+++ app/src/main/java/com/example/hwr_huschka/Constants.java
@@ -29,37 +29,38 @@ public class Constants {
 
     // User params
     public static final String REQ_PARAM_USERID = "UserID";
-    public static final String REQ_PARAM_USER_MAIL = "mail";
-    public static final String REQ_PARAM_USER_PASSWORD = "password";
-    public static final String REQ_PARAM_USER_FIRSTNAME = "firstname";
-    public static final String REQ_PARAM_USER_FAMILYNAME = "familyname";
-    public static final String REQ_PARAM_USER_ADDRESS = "adresse";
+    public static final String REQ_PARAM_USER_MAIL = "Email";
+    public static final String REQ_PARAM_USER_PASSWORD = "Password";
+    public static final String REQ_PARAM_USER_FIRSTNAME = "Vorname";
+    public static final String REQ_PARAM_USER_FAMILYNAME = "Nachname";
+    public static final String REQ_PARAM_USER_ADDRESS = "Adresse";
 
     // Product params
-    public static final String REQ_PARAM_PRODUCTID = "ProductID";
+    public static final String REQ_PARAM_PRODUCTID = "ProduktID";
     public static final String REQ_PARAM_PRODUCT_ARRAY = "ProductArray";
-    public static final String REQ_PARAM_PRODUCT_TEILNAME = "name";
+    public static final String REQ_PARAM_PRODUCT_TEILNAME = "Produktname";
 
 
 
     // return names from String Request
     public static final String REQ_RETURN_SHOPPINGLISTID = "ListenID";
-    public static final String REQ_RETURN_SHOPPINGLIST_DATUM = "date";
-    public static final String REQ_RETURN_SHOPPINGLIST_SUPERMARKT = "supermarkt";
+    public static final String REQ_RETURN_SHOPPINGLIST_DATUM = "Erstelldatum";
+    public static final String REQ_RETURN_SHOPPINGLIST_SUPERMARKT = "Supermarkt";
     public static final String REQ_RETURN_SHOPPINGLIST_STATUS = "status";
     public static final String REQ_RETURN_SHOPPINGLIST_NUMBEROF_PRODUCTS = "Anzahl";
+    public static final String REQ_RETURN_SHOPPINGLIST_PRICE = "Gesamtpreis";
 
-    public static final String REQ_RETURN_USERID = "KundenID";
-    public static final String REQ_RETURN_USER_MAIL = "mail";
-    public static final String REQ_RETURN_USER_FIRSTNAME = "firstname";
-    public static final String REQ_RETURN_USER_FAMILYNAME = "familyname";
+    public static final String REQ_RETURN_USERID = "UserID";
+    public static final String REQ_RETURN_USER_MAIL = "Email";
+    public static final String REQ_RETURN_USER_FIRSTNAME = "Vorname";
+    public static final String REQ_RETURN_USER_FAMILYNAME = "Nachname";
 
-    public static final String REQ_RETURN_PRODUKTID = "ProductID";
-    public static final String REQ_RETURN_PRODUKT_NAME = "produktname";
-    public static final String REQ_RETURN_PRODUKT_PRODUCER = "hersteller";
-    public static final String REQ_RETURN_PRODUKT_PRICE = "preis";
-    public static final String REQ_RETURN_PRODUKT_KCAL = "kcal";
-    public static final String REQ_RETURN_PRODUKT_KATEGORIE = "kategorie";
+    public static final String REQ_RETURN_PRODUKTID = "ProduktID";
+    public static final String REQ_RETURN_PRODUKT_NAME = "Produktname";
+    public static final String REQ_RETURN_PRODUKT_PRODUCER = "Hersteller";
+    public static final String REQ_RETURN_PRODUKT_PRICE = "Preis";
+    public static final String REQ_RETURN_PRODUKT_KCAL = "Kcal";
+    public static final String REQ_RETURN_PRODUKT_KATEGORIE = "Kategorie";
 
 
 
