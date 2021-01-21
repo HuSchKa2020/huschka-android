b14a156e0c9f45b58f9e1730110378e44158879b
derSchumann <der.schumann194@gmx.de>
2021-01-21 15:20:16 +0100

    activity erstellt und toolbar funktion erstellt

---
+++ app/src/main/java/com/example/hwr_huschka/Activities/GoShoppingActivity.java
@@ -0,0 +1,40 @@
+package com.example.hwr_huschka.Activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.hwr_huschka.R;
+
+public class GoShoppingActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_shopping);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case (android.R.id.home):
+                finish();
+                break;
+        }
+
+        return true;
+    }
+}
