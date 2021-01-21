33947194fb79bdd7fa46ef9c2cfc1f85c7beb9b1
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-01-21 17:19:28 +0100

    Klasse Produkt das in der Einkaufsliste ist
    ist einfach eiN produkt mit einer weiteren EIgenschaft Anzahl

---
+++ app/src/main/java/com/example/hwr_huschka/klassen/ProductInShoppinglist.java
@@ -0,0 +1,19 @@
+package com.example.hwr_huschka.klassen;
+
+public class ProductInShoppinglist extends Product{
+
+    int anzahl;
+
+    public ProductInShoppinglist(int produktID, String hersteller, String name, String kategorie, double preis, int kcal, int anzahl) {
+        super(produktID, hersteller, name, kategorie, preis, kcal);
+        this.anzahl = anzahl;
+    }
+
+    public int getAnzahl() {
+        return anzahl;
+    }
+
+    public void setAnzahl(int anzahl) {
+        this.anzahl = anzahl;
+    }
+}
