bccef76e49b759627ea32ff6b002efe4b9516d39
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-02-08 21:33:01 +0100

    property "ausgewählt" zur klasse hinzugefügt

--- app/src/main/java/com/example/hwr_huschka/klassen/ProductInShoppinglist.java
+++ app/src/main/java/com/example/hwr_huschka/klassen/ProductInShoppinglist.java
@@ -3,10 +3,21 @@ package com.example.hwr_huschka.klassen;
 public class ProductInShoppinglist extends Product{
 
     int anzahl;
+    boolean ausgewaehlt;
 
     public ProductInShoppinglist(int produktID, String hersteller, String name, String kategorie, double preis, int kcal, Position position, int anzahl) {
         super(produktID, hersteller, name, kategorie, preis, kcal, position);
+        ausgewaehlt = false;
         this.anzahl = anzahl;
+    }
+
+
+    public boolean isAusgewaehlt() {
+        return ausgewaehlt;
+    }
+
+    public void setAusgewaehlt(boolean ausgewaehlt) {
+        this.ausgewaehlt = ausgewaehlt;
     }
 
     public int getAnzahl() {
